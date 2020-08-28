ef3f0733a108089d9da507b611ae0e3c47d601a1
Yu Sun <yu.sun.cs@gmail.com>
2020-08-28 12:49:03 -0700

    update

--- checkout/control.js
+++ checkout/control.js
@@ -4,6 +4,28 @@ angular.module('v3App', ['ngClickCopy'])
   })
   .controller('MessageController', function($scope, $location) {
     
+    var url = $location.$$absUrl;
+    var url = new URL(url);
+    var stuName = url.searchParams.get("name");
+    var studentId = url.searchParams.get("studentId");
+    var courseId = url.searchParams.get("courseId");
+    var projectUrl = url.searchParams.get("projectUrl");
+    var nextCourse = url.searchParams.get("nextCourse");
+
+    console.log(stuName);
+    console.log(studentId);
+    console.log(courseId);
+    console.log(projectUrl);
+    console.log(nextCourse);
+
+    if (nextCourse != null) {
+    	$scope.nextCourse = nextCourse;
+    }
+
+    $scope.name = stuName;
+    $scope.summaryURL = "https://codingmindsacademy.com/feedback_ECv3.html?studentId=" + studentId + "&courseId=" + courseId;
+    $scope.projectURL = projectUrl;
+
   	$scope.reset = function () {
   		console.log("clicked");
   	}
