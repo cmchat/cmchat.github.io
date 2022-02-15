c33a8b0898ee2265831079ca6c298ae8e25d5f09
yujiazhang715 <81319388+yujiazhang715@users.noreply.github.com>
2022-02-14 21:32:29 -0800

    Update control.js

--- st-project/control.js
+++ st-project/control.js
@@ -1,35 +1,32 @@
-angular.module('v3App', ['ngClickCopy'])
+angular.module('v3App', ['ngclipboard'])
   .config(function($sceProvider) {
     $sceProvider.enabled(false);    
   })
   .controller('MessageController', function($scope, $location) {
     
-    /*var url = $location.$$absUrl;
-    //var url = new URL(url);    
-    //var studentId = url.searchParams.get("studentId");
-    //var projectUrl = url.searchParams.get("projectUrl");
+    var url = $location.$$absUrl;
+    var url = new URL(url);
+    var stuName = url.searchParams.get("name");
+    var studentId = url.searchParams.get("studentId");
+    var projectUrl = url.searchParams.get("projectUrl");
     var courseId = url.searchParams.get("courseId");
     var instructorName = url.searchParams.get("instructorName");
     var nextCourse = url.searchParams.get("nextCourse");    
-    var sid = url.searchParams.get("sid");*/
-    
-    var url = $location.$$absUrl;
-    var url = new URL(url);    
-    var stuName = url.searchParams.get("name");
-    var instructorName = url.searchParams.get("instructorName");
-    var conferenceName = url.searchParams.get("conferenceName");
-    var conferenceWeb = url.searchParams.get("conferenceWeb");
-    var conferenceCategory = url.searchParams.get("conferenceCategory");
+    var sid = url.searchParams.get("sid");
 
-    //if (nextCourse != null) {
-    	//$scope.nextCourse = nextCourse;
-    //}
+    console.log(stuName);
+    console.log(studentId);
+    console.log(courseId);
+    console.log(projectUrl);
+    console.log(nextCourse);
+    console.log(sid);
+
+    if (nextCourse != null) {
+    	$scope.nextCourse = nextCourse;
+    }
 
     $scope.name = stuName;    
     $scope.instructorName = instructorName;
-    $scope.conferenceName = conferenceName;
-    $scope.conferenceWeb = conferenceWeb;
-    $scope.conferenceCategory = conferenceCategory;
     
   	$scope.reset = function () {
   		console.log("clicked");
